1ac13f25c513ceaab480e4a48e58368f9b51511b
mohammed roshdy <mohamed.roshdy.twfyq@gmail.com>
2023-10-21 19:40:45 +0300

    تغير حجم الصورة

--- youtube/les4.py
+++ youtube/les4.py
@@ -1,3 +1,7 @@
 #تغير حجم الصورة واالنافزة
 import cv2
 img=cv2.imread('youtube\imges\car.jpg')
+new_imge=cv2.resize(img,(500,200))
+cv2.imshow("car",img)
+cv2.imshow('car cut',new_imge)
+cv2.waitKey(0)
